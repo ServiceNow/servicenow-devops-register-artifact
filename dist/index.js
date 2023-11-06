c457a3eeaa4262594658f2380359d4b4bd60e98d
kunal.gangaher <kunal.gangaher@servicenow.com>
2023-11-06 19:58:59 +0530

    STRY56331515: Logging response object at different levels.

--- dist/index.js
+++ dist/index.js
@@ -5274,7 +5274,11 @@ const axios = __nccwpck_require__(992);
         } else if (e.message.includes('401')) {
             core.setFailed('Invalid username and password or Invalid token and toolid. Please correct the input parameters and try again.');
             core.debug('[ServiceNow DevOps] Error: '+JSON.stringify(e));
-            if(e.response) core.debug('[ServiceNow DevOps] Response object :',e.response);
+            if(e.response) 
+            {
+                console.error('e.response in console error :',e.response);
+                core.debug('[ServiceNow DevOps] Response object :',e.response);
+            }
         } else if(e.message.includes('400') || e.message.includes('404')){
             let errMsg = '[ServiceNow DevOps] Artifact Registration is not Successful. ';
             let errMsgSuffix = ' Please provide valid inputs.';
--- src/main.js
+++ src/main.js
@@ -87,7 +87,11 @@ const axios = require('axios');
         } else if (e.message.includes('401')) {
             core.setFailed('Invalid username and password or Invalid token and toolid. Please correct the input parameters and try again.');
             core.debug('[ServiceNow DevOps] Error: '+JSON.stringify(e));
-            if(e.response) core.debug('[ServiceNow DevOps] Response object :',e.response);
+            if(e.response) 
+            {
+                console.error('e.response in console error :',e.response);
+                core.debug('[ServiceNow DevOps] Response object :',e.response);
+            }
         } else if(e.message.includes('400') || e.message.includes('404')){
             let errMsg = '[ServiceNow DevOps] Artifact Registration is not Successful. ';
             let errMsgSuffix = ' Please provide valid inputs.';
