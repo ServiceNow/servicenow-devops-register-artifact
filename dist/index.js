38485992ba390ec9f5b9627331f9cf9e89633886
kunal.gangaher <kunal.gangaher@servicenow.com>
2023-11-06 19:15:59 +0530

    STRY56331515: Adding logs for response object after stringify.

--- dist/index.js
+++ dist/index.js
@@ -5274,6 +5274,7 @@ const axios = __nccwpck_require__(992);
         } else if (e.message.includes('401')) {
             core.setFailed('Invalid username and password or Invalid token and toolid. Please correct the input parameters and try again.');
             core.debug('[ServiceNow DevOps] Error: '+JSON.stringify(e));
+            if(e.response) core.debug('[ServiceNow DevOps] Response object String :'+JSON.stringify(e.response));
             if(e.response) core.debug('[ServiceNow DevOps] Response object :'+JSON.stringify(e.response));
         } else if(e.message.includes('400') || e.message.includes('404')){
             let errMsg = '[ServiceNow DevOps] Artifact Registration is not Successful. ';
