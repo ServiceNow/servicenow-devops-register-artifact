364273a73c8f3282ace69cb18b79446cff1d21a1
kunal.gangaher <kunal.gangaher@servicenow.com>
2023-11-07 17:41:45 +0530

    STRY56331515: Changing the place of logging response data.

--- src/main.js
+++ src/main.js
@@ -95,15 +95,15 @@ function circularSafeStringify(obj) {
         }
     } catch (e) {
         core.debug('[ServiceNow DevOps] Artifact Registration, Error: '+JSON.stringify(e));
+        if(e.response && e.response.data) {
+            var responseObject=circularSafeStringify(e.response);
+            core.debug('[ServiceNow DevOps] Artifact Registration, Response data :'+responseObject);          
+        }
+
         if (e.message.includes('ECONNREFUSED') || e.message.includes('ENOTFOUND') || e.message.includes('405')) {
             core.setFailed('ServiceNow Instance URL is NOT valid. Please correct the URL and try again.');
         } else if (e.message.includes('401')) {
             core.setFailed('Invalid username and password or Invalid token and toolid. Please correct the input parameters and try again.');
-            if(e.response && e.response.data) 
-            {
-                var responseObject=circularSafeStringify(e.response.data);
-                core.debug('[ServiceNow DevOps] Artifact Registration, Response data :'+responseObject);          
-            }
         } else if(e.message.includes('400') || e.message.includes('404')){
             let errMsg = '[ServiceNow DevOps] Artifact Registration is not Successful. ';
             let errMsgSuffix = ' Please provide valid inputs.';
