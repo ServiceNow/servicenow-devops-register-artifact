e2238acfe074ee00b1de036e811e6aef44dbf31e
kunal.gangaher <kunal.gangaher@servicenow.com>
2023-11-22 19:26:33 +0530

    STRY56331536: Correcting the technique to fetch the status code.

--- dist/index.js
+++ dist/index.js
@@ -5287,8 +5287,8 @@ function circularSafeStringify(obj) {
     } catch (e) {
         core.debug('[ServiceNow DevOps] Artifact Registration, Error: '+JSON.stringify(e));
         if(e.response && e.response.data) {
-            var responseObject=circularSafeStringify(e.response);
-            core.debug('[ServiceNow DevOps] Artifact Registration, Status code :'+e.response.statusCode+', Response data :'+responseObject);          
+            var responseObject=circularSafeStringify(e.response.data);
+            core.debug('[ServiceNow DevOps] Artifact Registration, Status code :'+e.response.status+', Response data :'+responseObject);          
         }
 
         if (e.message.includes('ECONNREFUSED') || e.message.includes('ENOTFOUND') || e.message.includes('405')) {
