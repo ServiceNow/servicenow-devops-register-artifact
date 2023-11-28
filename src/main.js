0e3a7859c16a5dfba207ad67b784c174d869e1d0
kunal.gangaher <kunal.gangaher@servicenow.com>
2023-11-28 15:50:46 +0530

    STRY56331536: Adding data of response in log.

--- src/main.js
+++ src/main.js
@@ -92,7 +92,7 @@ function circularSafeStringify(obj) {
             httpHeaders = { headers: defaultHeadersForBasicAuth };
             core.debug("[ServiceNow DevOps], Sending Request for Artifact Registration, Request options :"+JSON.stringify(httpHeaders)+", Payload :"+JSON.stringify(payload)+"\n");
             snowResponse = await axios.post(endpoint, JSON.stringify(payload), httpHeaders);
-            core.debug("[ServiceNow DevOps], Receiving response for Artifact Registration, Response :"+JSON.stringify(snowResponse)+"\n");
+            core.debug("[ServiceNow DevOps], Receiving response for Artifact Registration, Response :"+JSON.stringify(snowResponse.data)+"\n");
         }
         else {
             core.setFailed("For Basic Auth, Username and Password is mandatory for integration user authentication");
